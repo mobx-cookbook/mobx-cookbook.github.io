f35c61692433cfdc307196e6d452f4e896ead941
zapolnoch <zapolnoch@gmail.com>
2022-07-04 12:21:06 +0300

    :sparkles: add docusaurus

---
+++ sidebars.js
@@ -0,0 +1,46 @@
+// @ts-check
+
+/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
+const sidebars = {
+  tutorialSidebar: [
+    {
+      type: "category",
+      label: "Введение",
+      items: [
+        "installation",
+        "classes",
+        "observable-state",
+        "reactions",
+        {
+          type: "category",
+          label: "Actions",
+          link: { type: "doc", id: "actions" },
+          items: ["actions", "async-actions"],
+        },
+        {
+          type: "category",
+          label: "Доступ к состоянию",
+          link: { type: "doc", id: "react-integration/react-integration" },
+          items: [
+            "react-integration/singleton",
+            "react-integration/context-api",
+          ],
+        },
+      ],
+    },
+    {
+      type: "category",
+      label: "Практика",
+      link: { type: "doc", id: "practice" },
+      items: ["practice"],
+    },
+    {
+      type: "category",
+      label: "Продвинутые темы",
+      link: { type: "doc", id: "advanced" },
+      items: ["render-optimizations"],
+    },
+  ],
+}
+
+module.exports = sidebars
